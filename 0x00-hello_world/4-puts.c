697a171208cee3363fdfdfbdc5c5802115af0f70
kris <christopherugwu3@gmail.com>
2022-06-30 10:17:22 -0700

    mnjh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,12 @@
  *
  * Return: Alway 0 (Success)
  */
-
-
 int main(void)
+
 {
-	puts("/ "Programming is like building a multilingual puzzl");
-	return (0);
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
