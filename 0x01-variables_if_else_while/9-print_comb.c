be1f38b0c2f4fab69dfa4cad4a2d65bc8813a284
kris <christopherugwu3@gmail.com>
2022-07-04 11:08:08 -0700

    mmjk

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,13 +2,14 @@
 /**
  * main - combination of number
  *
- * Return: Always (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 int c;
 
 for (c = '0'; c <= '9'; c++)
+
 {
 putchar(c);
 if (c != '9');
