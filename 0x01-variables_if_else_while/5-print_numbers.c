924d1d9c7b47d0a3d0e628803e3b028da0e63633
kris <christopherugwu3@gmail.com>
2022-07-04 02:55:04 -0700

    mmnb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - print digit number
+ *
+ * Return: Always (success)
+ */
+int main(void)
+{
+char n;
+for (n = '0'; n <= '9'; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
