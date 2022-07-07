6d69f4294481f0f62f125649a80d8c614d2cf071
kris <christopherugwu3@gmail.com>
2022-07-07 16:26:27 -0700

    lllll

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - print digits
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+char c;
+for (c >= 48; c <= 57; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
