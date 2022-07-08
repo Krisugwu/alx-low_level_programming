beccf3dc7ab864c686c1a4a0e9407c0accee3492
kris <christopherugwu3@gmail.com>
2022-07-07 17:22:10 -0700

    mmj

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,11 +6,17 @@
  */
 void print_most_numbers(void)
 {
-int ch;
-for (c = 48; c > 58; ch++)
+int ch = 0;
+for (ch <= 9; ch++)
 {
-if (c != 50 && c != 52)
-_putchar(ch);
+if (ch == 2 || ch == 4)
+{
+continue;
+}
+else
+{
+_putchar(x + '0')
+}
 }
 _putchar('\n');
 }
