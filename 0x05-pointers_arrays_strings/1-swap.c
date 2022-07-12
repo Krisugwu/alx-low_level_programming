ab1571c4173dfe96f06b133979a64a218b81e5a2
kris <christopherugwu3@gmail.com>
2022-07-12 04:33:07 -0700

    hhu

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - swap integers
+ * @a: first integer
+ * @b: second interger
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
