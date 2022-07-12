38df4352196dc6f341711bbc1cf9ccdf5cd4d2aa
kris <christopherugwu3@gmail.com>
2022-07-12 04:45:42 -0700

    llo

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - return lenght of a string
+ * @s:input of string
+ * Return: lenght of string
+ */
+int _strlen(char *s)
+{
+int count = 0
+{
+while (*(s + count) != '/0')
+	count++;
+return (count);
+}
