90be086b1d7ef9fd28731b875332e9842723f076
kris <christopherugwu3@gmail.com>
2022-07-20 07:45:23 -0700

    llop

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,11 +5,10 @@
  * @c: iterator.
  * Return: 1 if n is a prime number. 0 if n is not a prime number.
  */
-  int is_prime(unsigned int n, unsigned int c)
+int is_prime(unsigned int n, unsigned int c)
 {
 if (n % c == 0)
 {
-
 if (n == c)
 return (1);
 else
